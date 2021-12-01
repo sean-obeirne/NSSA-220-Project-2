1d21933995d67aeeb65d6834636b26e34bfb1c5e
damp-waffle <maniperez67@gmail.com>
2021-12-01 17:18:06 -0500

    Updated notes on metrics

--- Code/packet_analyzer.py
+++ Code/packet_analyzer.py
@@ -1,7 +1,8 @@
 #from filter_packets import *
 from packet_parser import *
 from compute_metrics import *
-filename = "../Captures/Node1.txt"
+filename = sys.argv[1]
 list = parse(filename)
 #filter()
 compute(list)
+#print(list)
