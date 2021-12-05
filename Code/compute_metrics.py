4f7a14c16074d72b9b31482c026d20636a239588
Sean O'Beirne <sean.t.obeirne@gmail.com>
2021-12-05 07:00:28 -0500

    finished compute metrics implementation

--- Code/compute_metrics.py
+++ Code/compute_metrics.py
@@ -1,8 +1,10 @@
-# need this function to left-pad millisecond data with zeros when millisecond number is small
+# need this function to left-pad millisecond data with zeros (sometimes)
+# needed when millisecond number is small
+# otherwise decimal places are off during rtt calculation (subtraction)
 def hex_to_timestamp(hexsecs, hexmils):
     seconds = str(int(hexsecs, 16))
     milliseconds = str(int(hexmils, 16))
-    while len(milliseconds) < 6:
+    while len(milliseconds) < 6: # max millisecond hex string has 6 characters
         milliseconds = "0" + milliseconds
     return float(seconds + "." + milliseconds)
     
@@ -16,6 +18,9 @@ def hex_to_timestamp(hexsecs, hexmils):
 #   5 - destination ip
 #   6 - ICMP type
 def compute(metrics, node):
+#
+# Initialize Variables
+#
     timestamps = list()
     ttl = 0
     rtt = 0
@@ -41,40 +46,41 @@ def compute(metrics, node):
         ip = "c0a8c802"
     else:
         print("Error: no IP known for Node " + str(node))
-        exit(1)
+        return
+
+#
+# Calculate Data from Metrics
+#
     for z in metrics:
-        # timestamps.append(float( str(int(z[0], 16)) + "." + str(float(z[1], 16)) ))
         timestamps.append(hex_to_timestamp(z[0], z[1]))
-        # print("this timestamp: " + str(timestamps[i]))
+
         if ip in str(z[4]):
             if echo_req in z[6]: # we sent an echo request
                 req_sent += 1
                 req_bytes_sent += int(z[2], 16)
             else: # we sent an echo reply 
                 rep_sent += 1
-                if echo_req in metrics[i-1][6] and ip in metrics[i-1][5]:
-                    reply_delay += (timestamps[i] - timestamps[i-1])
-                else:
-                    print("WARNING: MISMATCH IN REQUEST TO REPLY, USING NEXT VALUE")
-                    reply_delay += (timestamps[i] - timestamps[i-2])
-                    
+                reply_delay += (timestamps[i] - timestamps[i-1])
+
         else:
             if echo_req in z[6]: # we received an echo request
                 req_rec +=1
                 req_bytes_rec += int(z[2], 16)
             else: # we received an echo reply
                 rep_rec += 1
-                if echo_req in metrics[i-1][6] and ip in metrics[i-1][4]:
-                    rtt += timestamps[i] - timestamps[i-1]
-                    ttl += 1 
-                else:
-                    print("WARNING: MISMATCH IN REQUEST TO REPLY, USING NEXT VALUE")
-                    rtt += timestamps[i] - timestamps[i-2]
+                rtt += timestamps[i] - timestamps[i-1]
+                ttl += int(metrics[i-1][3], 16) - int(z[3], 16) + 1
+
         i += 1
+
+#
+# Output Data
+#
     req_data_sent = req_bytes_sent - (42 * req_sent)
     req_data_rec = req_bytes_rec - (42 * req_rec)
     rttavg = round((1000 * rtt) / rep_rec, 2)
     avg_reply_delay = round((1000000 * reply_delay) / rep_sent, 2)
+    avg_ttl = round(ttl / rep_rec, 2)
     print("Requests sent: " + str(req_sent))
     print("Request Recieved: " + str(req_rec))
     print("Replies Sent: " + str(rep_sent))
@@ -87,4 +93,4 @@ def compute(metrics, node):
     print("Echo Request Throughput (kB/sec): " + str( round((req_bytes_sent/1000) / rtt, 1) ))
     print("Echo Request Goodput (kB/sec): " + str( round((req_data_sent /1000) / rtt, 1) ))
     print("Average Reply Delay (us): " + str(avg_reply_delay))
-    print("Average Echo Request Hop Count: " + str())
+    print("Average Echo Request Hop Count: " + str(avg_ttl))
